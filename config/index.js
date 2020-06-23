1431d6f47e61dd16ff54eb773b50a890a6b8bb26
mikamihero <mikamihero@gmail.com>
2020-06-23 16:44:35 +1000

    remove the dependence on config.json (causing too many headaches)

--- config/index.js
+++ config/index.js
@@ -1,12 +1,10 @@
-const config = require("./config.json");
-
 // Config taken from environment variables
 config.nodeEnv = process.env.NODE_ENV;
 
 // Config taken from config.json file
-config.mongodbURI = process.env.MONGODB_URI || config[config.nodeEnv].mongodbURI;
-config.mailGunDomain = process.env.MAILGUN_DOMAIN || config[config.nodeEnv].mailGunDomain;
-config.mailGunAPIKey = process.env.MAILGUN_API_KEY || config[config.nodeEnv].mailGunAPIKey;
-config.mailGunToEmail = process.env.MAILGUN_TO_EMAIL || config[config.nodeEnv].mailGunToEmail;
+config.mongodbURI = process.env.MONGODB_URI;
+config.mailGunDomain = process.env.MAILGUN_DOMAIN;
+config.mailGunAPIKey = process.env.MAILGUN_API_KEY;
+config.mailGunToEmail = process.env.MAILGUN_TO_EMAIL;
 
 module.exports = config;
