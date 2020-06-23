06da8080ff818979b71b92e9a57f6ae9c54b63b9
mikamihero <mikamihero@gmail.com>
2020-06-23 16:33:38 +1000

    Rejigging some of the env vars

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ config.nodeEnv = process.env.NODE_ENV;
 // Config taken from config.json file
 config.mongodbURI = process.env.MONGODB_URI || config[config.nodeEnv].mongodbURI;
 config.mailGunDomain = process.env.MAILGUN_DOMAIN || config[config.nodeEnv].mailGunDomain;
-config.mailGunAPIKey = process.env.MAILGUN_API || config[config.nodeEnv].mailGunAPIKey;
-config.mailGunToEmail = process.env.MAILGUN_TOEMAIL || config[config.nodeEnv].mailGunToEmail;
+config.mailGunAPIKey = process.env.MAILGUN_API_KEY || config[config.nodeEnv].mailGunAPIKey;
+config.mailGunToEmail = process.env.MAILGUN_TO_EMAIL || config[config.nodeEnv].mailGunToEmail;
 
 module.exports = config;
