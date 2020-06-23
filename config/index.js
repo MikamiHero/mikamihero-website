656f1a00685bd60610a07e364426d1558ce14e54
mikamihero <mikamihero@gmail.com>
2020-06-23 15:58:44 +1000

    changing the order of config index.js (reading from env var first before config.json)

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ const config = require("./config.json");
 config.nodeEnv = process.env.NODE_ENV;
 
 // Config taken from config.json file
-config.mongodbURI = config[config.nodeEnv].mongodbURI || process.env.MONGODB_URI;
-config.mailGunDomain = config[config.nodeEnv].mailGunDomain || process.env.MAILGUN_DOMAIN;
-config.mailGunAPIKey = config[config.nodeEnv].mailGunAPIKey || process.env.MAILGUN_API;
-config.mailGunToEmail = config[config.nodeEnv].mailGunToEmail || process.env.MAILGUN_TOEMAIL;
+config.mongodbURI = process.env.MONGODB_URI || config[config.nodeEnv].mongodbURI;
+config.mailGunDomain = process.env.MAILGUN_DOMAIN || config[config.nodeEnv].mailGunDomain;
+config.mailGunAPIKey = process.env.MAILGUN_API || config[config.nodeEnv].mailGunAPIKey;
+config.mailGunToEmail = process.env.MAILGUN_TOEMAIL || config[config.nodeEnv].mailGunToEmail;
 
 module.exports = config;
