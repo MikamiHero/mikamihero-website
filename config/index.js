7c726112cf8170c5ba8055ee223d87de24705884
mikamihero <mikamihero@gmail.com>
2020-06-23 15:57:03 +1000

    Changing the config index.js file a bit so it picks up env vars as well

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ const config = require("./config.json");
 config.nodeEnv = process.env.NODE_ENV;
 
 // Config taken from config.json file
-config.mongodbURI = config[config.nodeEnv].mongodbURI;
-config.mailGunDomain = config[config.nodeEnv].mailGunDomain;
-config.mailGunAPIKey = config[config.nodeEnv].mailGunAPIKey;
-config.mailGunToEmail = config[config.nodeEnv].mailGunToEmail;
+config.mongodbURI = config[config.nodeEnv].mongodbURI || process.env.MONGODB_URI;
+config.mailGunDomain = config[config.nodeEnv].mailGunDomain || process.env.MAILGUN_DOMAIN;
+config.mailGunAPIKey = config[config.nodeEnv].mailGunAPIKey || process.env.MAILGUN_API;
+config.mailGunToEmail = config[config.nodeEnv].mailGunToEmail || process.env.MAILGUN_TOEMAIL;
 
 module.exports = config;
