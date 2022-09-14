3899f4531a12f857f12c1b4112b680a0f19c1dc2
mikamihero <21453519+MikamiHero@users.noreply.github.com>
2022-09-14 10:12:59 +1000

    Setting up some of the components and CSS

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,6 +3,14 @@
     and allow us to simply add a reference to the CSS in /styles
     and have them applied globally across the entire app
 */
+
+// This CSS is for the layout of the app
+import "../styles/layout.css";
+
+// CSS for components (e.g., header)
+import "../styles/components/header.css";
+import "../styles/components/footer.css";
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,12 +2,19 @@
     Next.js app looks for a /pages directory that holds
     all of our different paths for the our web app.
 */
-import { Component } from "react"
+import { Component } from "react";
+
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default class extends Component {
-    render() {
-        return (
-            <div>Your Next.js app</div>
-        )
-    }
+  render() {
+    return (
+      <div className="layout-wrapper">
+        <Header />
+        <div className="homepage-container">Hello world</div>
+        <Footer />
+      </div>
+    );
+  }
 }
