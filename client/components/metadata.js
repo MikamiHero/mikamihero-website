67e329b7500cdba840a161db437617f40d4e5d5f
MikamiHero <21453519+MikamiHero@users.noreply.github.com>
2022-09-18 16:52:29 +1000

    Added favicon

--- client/components/metadata.js
+++ client/components/metadata.js
@@ -8,6 +8,25 @@ export default class extends Component {
       <Head>
         {/* This <meta> element gives us control over things like dimensions and scaling of the web page. */}
         <meta name="viewpoint" content="width-device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
         {/* The <title> element helps us with things like the text in the browser tab, title for the page with search engines, etc */}
         <title>{this.props.title}</title>
         <meta name="description" content={this.props.metaDescription} />
